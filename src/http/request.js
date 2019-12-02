2437ab59e033c288b197f324a4b60f2f4c34df4a
Ertsul <linzeguang@heywoods.cn>
2019-12-02 16:34:22 +0800

    fix：修改 request 请求封装

--- src/http/request.js
+++ src/http/request.js
@@ -1,25 +1,59 @@
+/**
+ * 将 params 对象转化为 get 字符串形式
+ * @param {*} params
+ */
+function _dealParams(params) {
+  if (
+    Object.prototype.toString.call(params) != "[object Object]" &&
+    !Object.keys(params).length
+  ) {
+    return "";
+  }
+  let arr = [];
+  for (let key in params) {
+    if (params.hasOwnProperty(key)) {
+      const value = params[key];
+      arr.push(`${key}=${value}`);
+    }
+  }
+  const res = `?${arr.join("&")}`;
+  return res;
+}
+
 function request({
-  url = '',
+  url = "",
   data = {},
-  method = 'POST',
+  method = "POST",
   header = {
-    'content-type': 'application/json' // 默认值
+    "content-type": "application/json" // 默认值
   }
 }) {
   return new Promise(async (resolve, reject) => {
-    wx.request({
-      url,
-      data,
-      method,
-      header,
+    let requestObj = {};
+    if (method == "GET") {
+      url += _dealParams(data);
+      requestObj = {
+        url,
+        method: "GET"
+      };
+    } else if (method == "POST") {
+      requestObj = {
+        url,
+        data,
+        method: "post"
+      };
+    }
+    requestObj = {
+      ...requestObj,
       success(res) {
         resolve(res);
       },
       fail(err) {
         reject(err);
       }
-    })
-  })
+    };
+    wx.request(requestObj);
+  });
 }
 
 module.exports = request;

831dbc7b427bd937155824a05c64fb169c892067
Ertsul <linzeguang@heywoods.cn>
2019-12-02 18:08:25 +0800

    fix：添加登录 api

--- src/api/api.js
+++ src/api/api.js
@@ -1,23 +1,31 @@
-const request = require('../http/request');
-const baseConfig = require('../http/baseConfig');
+const request = require("../http/request");
+const baseConfig = require("../http/baseConfig");
 
 // get 例子
-const getList = request({
-  method: 'GET',
-  url: baseConfig.baseUrl + '/list?page=1&size=10',
-})
+const getList = params =>
+  request({
+    method: "GET",
+    url: baseConfig.baseUrl + "/list",
+    data: params
+  });
 
 // post 例子
-const updateInfo = request({
-  method: 'POST',
-  url: baseConfig.baseUrl + '/updateInfo',
-  data: {
-    name: 'Ertsul',
-    age: 23
-  }
-})
+const updateInfo = params =>
+  request({
+    method: "POST",
+    url: baseConfig.baseUrl + "/updateInfo",
+    data: params
+  });
+
+// 手机登录
+const loginApi = params =>
+  request({
+    method: "GET",
+    url: baseConfig.baseUrl + "/login/cellphone",
+    data: params
+  });
 
 module.exports = {
   getList,
   updateInfo
-}
+};

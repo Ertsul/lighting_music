bf82f730f7272050de5560fa711e0ae0370a1d19
Ertsul <179445828@qq.com>
2019-12-14 23:18:27 +0800

    feat：添加最近播放页面

--- src/router/routerPath.js
+++ src/router/routerPath.js
@@ -5,7 +5,8 @@ const routerPath = {
   index: "/pages/index/index",
   rank: "/pages/rank/rank",
   recommend: "/pages/recommend/recommend",
-  player: "/pages/player/player"
+  player: "/pages/player/player",
+  recentPlay: "/pages/recentPlay/recentPlay",
 }
 
 module.exports = routerPath;

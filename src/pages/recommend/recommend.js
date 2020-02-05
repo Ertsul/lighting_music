03fc7713b6b06376141bbaf7acd455f1747ec790
Ertsul <179445828@qq.com>
2020-02-05 23:33:36 +0800

    fix：实现推荐页面 UI

--- src/pages/recommend/recommend.js
+++ src/pages/recommend/recommend.js
@@ -1,1 +1,71 @@
-Page({})
+Page({
+  data: {
+    //   'http://img0.imgtn.bdimg.com/it/u=1563847232,2166245740&fm=26&gp=0.jpg',
+    //   'http://img4.imgtn.bdimg.com/it/u=2958967064,2714608608&fm=26&gp=0.jpg',
+    //   'http://img4.imgtn.bdimg.com/it/u=4013814910,1099586848&fm=26&gp=0.jpg',
+    //   'http://img5.imgtn.bdimg.com/it/u=441837680,1650038187&fm=26&gp=0.jpg
+    hotSongs: [
+      {
+        img: 'http://img5.imgtn.bdimg.com/it/u=441837680,1650038187&fm=26&gp=0.jpg',
+        songName: 'Lemon',
+        singer: '米津玄师'
+      },
+      {
+        img: 'http://img5.imgtn.bdimg.com/it/u=441837680,1650038187&fm=26&gp=0.jpg',
+        songName: 'Lemon',
+        singer: '米津玄师'
+      },
+      {
+        img: 'http://img5.imgtn.bdimg.com/it/u=441837680,1650038187&fm=26&gp=0.jpg',
+        songName: 'Lemon',
+        singer: '米津玄师'
+      },
+      {
+        img: 'http://img5.imgtn.bdimg.com/it/u=441837680,1650038187&fm=26&gp=0.jpg',
+        songName: 'Lemon',
+        singer: '米津玄师'
+      },
+      {
+        img: 'http://img5.imgtn.bdimg.com/it/u=441837680,1650038187&fm=26&gp=0.jpg',
+        songName: 'Lemon',
+        singer: '米津玄师'
+      },
+      {
+        img: 'http://img5.imgtn.bdimg.com/it/u=441837680,1650038187&fm=26&gp=0.jpg',
+        songName: 'Lemon',
+        singer: '米津玄师'
+      },
+    ],
+    recommendSongs: [
+      {
+        img: 'http://img4.imgtn.bdimg.com/it/u=4013814910,1099586848&fm=26&gp=0.jpg',
+        counts: 1000,
+        name: '歌单名歌单名歌单名歌单名歌单名歌单名歌单名'
+      },
+      {
+        img: 'http://img4.imgtn.bdimg.com/it/u=4013814910,1099586848&fm=26&gp=0.jpg',
+        counts: 1000,
+        name: '歌单名歌单名歌单名歌单名歌单名歌单名歌单名'
+      },
+      {
+        img: 'http://img4.imgtn.bdimg.com/it/u=4013814910,1099586848&fm=26&gp=0.jpg',
+        counts: 1000,
+        name: '歌单名歌单名歌单名歌单名歌单名歌单名歌单名'
+      }, {
+        img: 'http://img4.imgtn.bdimg.com/it/u=4013814910,1099586848&fm=26&gp=0.jpg',
+        counts: 1000,
+        name: '歌单名歌单名歌单名歌单名歌单名歌单名歌单名'
+      },
+      {
+        img: 'http://img4.imgtn.bdimg.com/it/u=4013814910,1099586848&fm=26&gp=0.jpg',
+        counts: 1000,
+        name: '歌单名歌单名歌单名歌单名歌单名歌单名歌单名'
+      },
+      {
+        img: 'http://img4.imgtn.bdimg.com/it/u=4013814910,1099586848&fm=26&gp=0.jpg',
+        counts: 1000,
+        name: '歌单名歌单名歌单名歌单名歌单名歌单名歌单名'
+      }
+    ]
+  }
+})
